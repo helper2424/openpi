8773c466a2805b0545cc8d6c1c4bf05c12039cfe
Eugene Mironov <helper2424@gmail.com>
2025-06-06 01:16:17 +0700

    fixup

--- scripts/serve_policy.py
+++ scripts/serve_policy.py
@@ -9,7 +9,7 @@ from openpi.policies import policy as _policy
 from openpi.policies import policy_config as _policy_config
 from openpi.serving import websocket_policy_server
 from openpi.training import config as _config
-
+import jax
 
 class EnvMode(enum.Enum):
     """Supported environments."""
